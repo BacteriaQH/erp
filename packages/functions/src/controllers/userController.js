e3881f6c339eaf537c19e385b581b5aac10ce2e5
vhphuc <hoangphuc2k1vp@gmail.com>
2023-12-01 09:52:41 +0700

    refactor: change logic in checkUserController

--- packages/functions/src/controllers/userController.js
+++ packages/functions/src/controllers/userController.js
@@ -4,22 +4,25 @@ export const checkUserController = async ctx => {
   try {
     const {email, avatar} = ctx.request.body;
     const user = await checkUser(email);
-    if (!user || user.role.length < 0 || !user.active) {
-      ctx.body = {
-        data: {},
-        success: true
-      };
-    }
-    if (user.avatar === '') {
-      const updatedUser = await updateUserById(user.id, {avatar});
+    if (user && user.role.length > 0 && user.active) {
+      if (user.avatar === '') {
+        const updatedUser = await updateUserById(user.id, {avatar});
+        return (ctx.body = {
+          data: updatedUser,
+          success: true
+        });
+      }
       return (ctx.body = {
-        data: updatedUser,
+        data: user,
         success: true
       });
     }
+    ctx.status = 403;
     ctx.body = {
-      data: user,
-      success: true
+      err: {
+        message: 'Forbidden'
+      },
+      success: false
     };
   } catch (e) {
     ctx.status = 404;
