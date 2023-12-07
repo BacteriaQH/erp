7b93c2291855a83996f6b27d9768eecbc1bc33e5
vhphuc <hoangphuc2k1vp@gmail.com>
2023-12-07 17:49:15 +0700

    feat: upload middleware

--- packages/functions/src/middleware/uploadMiddleware.js
+++ packages/functions/src/middleware/uploadMiddleware.js
@@ -1,10 +1,10 @@
-import multer from '@koa/multer';
+import multer from 'koa-multer-esm';
 import path from 'path';
 //options to limit file size and file extension
 
 let storage = multer.diskStorage({
   destination: function(req, file, callback) {
-    callback(null, './public/uploads');
+    callback(null, './public');
   },
   filename: function(req, file, callback) {
     callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
@@ -13,3 +13,12 @@ let storage = multer.diskStorage({
 export const upload = multer({
   storage: storage
 });
+
+export const uploadMiddleware = async (ctx, next) => {
+  try {
+    await upload.single('file');
+    await next();
+  } catch (e) {
+    console.log(e);
+  }
+};
