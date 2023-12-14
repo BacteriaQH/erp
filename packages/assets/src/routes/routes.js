49f07ac14e770fe256676ff9f023fbfbf022149a
vhphuc <hoangphuc2k1vp@gmail.com>
2023-12-14 08:19:15 +0700

    refactor: change router handle

--- packages/assets/src/routes/routes.js
+++ packages/assets/src/routes/routes.js
@@ -7,43 +7,39 @@ import NotFound from '@assets/loadables/NotFound';
 import User from '@assets/loadables/User';
 import {getStorageData} from '@assets/helpers/storage';
 import Profile from '@assets/loadables/Profile';
+import {ROLE} from '@assets/helpers/utils/validUrl';
 // eslint-disable-next-line react/prop-types
 const user = getStorageData('user');
-const router = {
-  admin: [
-    {
-      path: '/',
-      component: Home
-    },
-    {
-      path: '/settings',
-      component: Settings
-    },
-    {
-      path: '/user',
-      component: User
-    },
-    {
-      path: '/me',
-      component: Profile
-    }
-  ],
-  user: [
-    {
-      path: '/',
-      component: Home
-    },
-    {
-      path: '/me',
-      component: Profile
-    }
-  ]
-};
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    role: [ROLE.ADMIN, ROLE.USER]
+  },
+  {
+    path: '/settings',
+    component: Settings,
+    role: [ROLE.ADMIN]
+  },
+  {
+    path: '/user',
+    component: User,
+    role: [ROLE.ADMIN]
+  },
+  {
+    path: '/me',
+    component: Profile,
+    role: [ROLE.ADMIN, ROLE.USER]
+  }
+];
+
 const Routes = ({prefix = routePrefix}) => (
   <Switch>
-    {router[user?.user?.role].map((route, index) => (
-      <Route key={index} exact path={prefix + route.path} component={route.component} />
-    ))}
+    {routes
+      .filter(({role}) => role.some(r => user?.user?.role.includes(r)))
+      .map(({path, component}) => (
+        <Route key={path} exact path={`${prefix}${path}`} component={component} />
+      ))}
     <Route path="*" component={NotFound} />
   </Switch>
 );
